fdbec2f8312e21e7e6b2bff3211abf9021d612a2
sxc <18792733166@163.com>
2020-07-31 16:25:39 +0800

    完成了报表功能的开发

---
+++ src/components/report/Report.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <!--面包屑导航区域-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!--卡片视图区域-->
+    <el-card>
+      <!-- 2 .为 ECharts 准备一个具备大小（宽高）的 DOM -->
+      <div id="main" style="width: 750px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+//1. 导入echarts
+import echarts from "echarts";
+import _ from 'lodash'
+
+export default {
+  data() {
+    return {
+      //需要合并的数据
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    };
+  },
+  created() {},
+  methods: {},
+  //此时，页面上的元素，已经被渲染完毕了！
+  async mounted() {
+    //3 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById("main"));
+    
+    const {data:res } = await this.$http.get('reports/type/1');
+    if(res.meta.status !== 200){
+      return this.$message.error('获取折线图数据失败！');
+    }
+    //4 指定图表的配置项和数据
+    const result = _.merge(res.data,this.options);
+    //5.使用刚指定的配置项和数据显示图表。
+    myChart.setOption(result);
+  }
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import GoodsList from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(Router)
 
@@ -38,6 +39,7 @@ const router = new Router({
         { path:'/goods',component:GoodsList},
         { path:'/goods/add',component:Add},
         { path:'/orders',component:Order},
+        { path:'/reports',component:Report}
     ]}
   ]
 })
